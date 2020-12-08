a9a54262bcf6f391a55723e93ea3e04eab958d81
Scott Hardin <hardin.scott93@gmail.com>
2020-12-07 20:58:49 -0500

    fixed raw use of Event in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String[] ips = new String[]{"192.168.1.106", "192.168.1.107"};
         Handler wake = new WakeComputer();
-        Event<String> test = new Phone(5, EventType.connection, wake, ips);
+        Phone test = new Phone(5, EventType.connection, wake, ips);
         System.out.println(test.condition());
     }
 }
